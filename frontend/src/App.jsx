99c7a31550bc2abb8554d799f66461d731ea6cc0
Madhavan <ssmadhavan006@gmail.com>
2026-06-26 11:29:46 +0530

    update name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -156,7 +156,7 @@ export default function App() {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5" />
             </svg>
           </div>
-          <div className="logo-text">Antigravity CAD</div>
+          <div className="logo-text">Text to CAD</div>
         </div>
 
         <div className="sidebar-content">
